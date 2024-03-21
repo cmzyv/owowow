5659462e8ba1ff319a2e228198eb0e1c0c6666c9
Comzvy <44536997+cmzyv@users.noreply.github.com>
2024-03-21 21:34:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-507aa5b2-e658-4470-82c9-c8545e9e43a0/)
 
 
 echo "$curl_output" >> Update.tsx
